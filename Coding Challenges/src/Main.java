1d8422eda6061737c1cae2058d0ed71b3262ec03
romilxcode <romilxcode@gmail.com>
2025-12-05 14:26:58 +0530

    java\

---
+++ Coding Challenges/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    static void main(String[] args) {
+        System.out.println("Please enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Welcome " + name + " romilxcode");
+    }
+}
